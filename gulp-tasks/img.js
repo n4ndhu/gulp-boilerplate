168ed0ccf7dad9e3ab8ab4933300d8f5006188ea
Nandhu S Sasidharan <n4ndhu@gmail.com>
2017-09-17 13:19:00 +0530

    imgMin optimized

--- gulp-tasks/img.js
+++ gulp-tasks/img.js
@@ -2,14 +2,18 @@ module.exports = {
     dist: function(gulp, $, paths, imgMin) {
         return function() {
             gulp.src(paths.img.app)
+                .pipe($.wait(2000))
                 .pipe($.plumber())
+                .pipe($.changed(paths.img.dist))
                 .pipe($.imagemin(imgMin))
                 .pipe(gulp.dest(paths.img.dist))
+                .pipe($.browserSync.reload({ stream: true }));
         }
     },
     build: function(gulp, $, paths, imgMin) {
         return function() {
             gulp.src(paths.img.app)
+                .pipe($.wait(2000))
                 .pipe($.plumber())
                 .pipe($.imagemin(imgMin))
                 .pipe(gulp.dest(paths.img.build))
